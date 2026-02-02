9a27ab2a790ee2fa8a26e20dc0cf6b0622801e2c
Tala Aldhahri <toti_kd123@hotmail.com>
2026-02-02 17:00:17 +0300

    Initial Commit

---
+++ Talatna/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Talatna
+//
+//  Created by Tala Aldhahri on 14/08/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
