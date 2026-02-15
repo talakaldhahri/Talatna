a24b28f2e0864c73effb76ba0f775267014bce7c
Tala Aldhahri <toti_kd123@hotmail.com>
2026-02-15 20:30:40 +0300

    Create AutoGenViewModel

--- Talatna/Features/PlanBuilder/ViewModels/AutoGenViewModel.swift
+++ Talatna/Features/PlanBuilder/ViewModels/AutoGenViewModel.swift
@@ -5,3 +5,15 @@
 //  Created by Tala Aldhahri on 16/08/1447 AH.
 //
 
+import SwiftUI
+import Combine
+
+class AutoGenViewModel: ObservableObject {
+    //this variable represents the "form" that the user is filling out
+    @Published var preferences = PlanPreferences()
+    
+    //to validate the inputs: so that the user cannot move to the next screen without filling out the plan's name and categories
+    var isFormValid: Bool {
+        return !preferences.selectedCategories.isEmpty && !preferences.planName.isEmpty
+    }
+}
